b35f86b7988022fd8cddc2b2f2255ce6faf44549
wchen <sdjcw13@gmail.com>
2017-11-09 14:26:26 +0800

    test: move test case to java-common project.

--- src/test/java/com/avos/avoscloud/TestApp.java
+++ src/test/java/com/avos/avoscloud/TestApp.java
@@ -1,49 +1,36 @@
 package com.avos.avoscloud;
 
-import com.avos.avoscloud.BasicTest.TestDownloader;
-import com.avos.avoscloud.data.Armor;
-import com.avos.avoscloud.data.Player;
 import com.avos.avoscloud.internal.InternalConfigurationController;
+import com.avos.avoscloud.internal.InternalLogger;
+import com.avos.avoscloud.internal.impl.DefaultAppRouter;
 import com.avos.avoscloud.internal.impl.JavaAppConfiguration;
-import com.avos.avoscloud.internal.impl.JavaRequestSignImplementation;
 import com.avos.avoscloud.internal.impl.Log4j2Implementation;
 import com.avos.avoscloud.internal.impl.SimplePersistence;
 
 public class TestApp {
 
   public static void init() {
-    System.setProperty("LEANCLOUD_APP_HOOK_KEY", "rTXH9hotZfAJjtFOQqwfEHpC");
-    InternalConfigurationController.Builder builder = new InternalConfigurationController.Builder();
-    builder.setDownloaderImplementation(TestDownloader.class);
-    builder.setAppConfiguration(JavaAppConfiguration.instance())
-        .setInternalRequestSign(JavaRequestSignImplementation.instance())
+    TestApp.init("uu2P5gNTxGhjyaJGAPPnjCtJ-gzGzoHsz", "j5lErUd6q7LhPD8CXhfmA2Rg",
+        "atXAmIVlQoBDBLqumMgzXhcY", "rTXH9hotZfAJjtFOQqwfEHpC", true);
+  }
+
+  public static void init(String applicationId, String clientKey, String masterKey, String hookKey,
+      boolean isCN) {
+    System.setProperty("LEANCLOUD_APP_HOOK_KEY", hookKey);
+    JavaAppConfiguration configuration = JavaAppConfiguration.instance();
+    configuration.setIsCN(isCN);
+    configuration.setApplicationId(applicationId);
+    configuration.setClientKey(clientKey);
+    configuration.setMasterKey(masterKey);
+
+    InternalLogger logger = Log4j2Implementation.instance();
+    logger.setDebugEnabled(true);
+
+    new InternalConfigurationController.Builder().setAppConfiguration(configuration)
+        .setAppRouter(DefaultAppRouter.instance()).setInternalLogger(logger)
         .setInternalPersistence(SimplePersistence.instance())
-        .setInternalLogger(Log4j2Implementation.instance());
-    builder.build();
-
-    AVOSCloud.setDebugLogEnabled(true);
-    AVObject.registerSubclass(Armor.class);
-    AVObject.registerSubclass(Player.class);
-    AVOSCloud.useAVCloudCN();
-    AVOSCloud.initialize("uu2P5gNTxGhjyaJGAPPnjCtJ-gzGzoHsz", "j5lErUd6q7LhPD8CXhfmA2Rg",
-        "atXAmIVlQoBDBLqumMgzXhcY");
+        .build();
+    InternalConfigurationController.globalInstance().getAppRouter().updateServerHosts();
   }
 
-  public static void initForQCloud() {
-    System.setProperty("LEANCLOUD_APP_HOOK_KEY", "rTXH9hotZfAJjtFOQqwfEHpC");
-    AVOSCloud.setDebugLogEnabled(true);
-    AVObject.registerSubclass(Armor.class);
-    AVOSCloud.useAVCloudCN();
-    AVOSCloud.initialize("DV8dqMSRqujdz6hI7NFtCfEq-9Nh9j0Va", "IPvWcVpYBvkHuk6QYc9Jvg3F",
-        "atXAmIVlQoBDBLqumMgzXhcY");
-  }
-
-  public static void initForUS() {
-    System.setProperty("LEANCLOUD_APP_HOOK_KEY", "dCMnBqEygcursl7dtC2WmseV");
-    AVOSCloud.setDebugLogEnabled(true);
-    AVOSCloud.useAVCloudUS();
-    AVOSCloud.initialize("cswk4i7a7fgprutnxr9cldg6f7d9yr4jpsak2dxlm94vgaoy",
-        "7u1kavw2y2805kue7pxyxxszxyj46cbf3zxmde9n6exfpfpo",
-        "dtq0s5sjdf4op3nfnqtu2rqbk4yef6h0f1pysagtsjod8g2s");
-  }
 }
--- src/test/java/com/avos/avoscloud/data/AFTest.java
+++
@@ -1,7 +0,0 @@
-package com.avos.avoscloud.data;
-
-/**
- * Created by wli on 2017/6/29.
- */
-public class AFTest {
-}
